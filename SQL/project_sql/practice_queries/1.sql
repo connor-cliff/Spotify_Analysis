57dbd220cba071e835ce6563c29aff0d18da7cfc
Connor Cliff <connorcliff211@gmail.com>
2026-02-15 13:09:13 +0000

    Practice SQL commit

--- SQL/project_sql/practice_queries/1.sql
+++ SQL/project_sql/practice_queries/1.sql
@@ -20,5 +20,80 @@ ORDER BY track_popularity DESC
 LIMIT 10;
 
 --Q4
-SELECT AVG(track_duration_min)
+SELECT album_name, ROUND(AVG(track_duration_min)::numeric, 2) AS average_track_duration
 FROM tracks
+LEFT JOIN albums ON tracks.album_key = albums.album_key
+GROUP BY album_name;
+
+--Q5
+SELECT COUNT(DISTINCT(artist_key))
+FROM artists;
+
+--Q6 !!!
+SELECT * FROM (
+    SELECT 'min' AS type, album_name, album_release_date
+    FROM albums
+    WHERE album_name IS NOT NULL
+    ORDER BY album_release_date ASC
+    LIMIT 1
+) AS oldest
+
+UNION ALL
+
+SELECT * FROM (
+    SELECT 'max' AS type, album_name, album_release_date
+    FROM albums
+    WHERE album_name IS NOT NULL
+    ORDER BY album_release_date DESC
+    LIMIT 1
+) AS newest;
+
+--Q7
+SELECT AVG(artist_popularity), albums.album_type
+FROM artists
+LEFT JOIN albums ON albums.artist_key = artists.artist_key
+WHERE artist_name IS NOT NULL
+GROUP BY albums.album_type
+LIMIT 10
+
+---Level 2---
+--Q8
+
+SELECT artist_name, AVG(track_popularity) AS avg_pop, COUNT(DISTINCT(track_name)) AS track_count
+FROM artists
+LEFT JOIN albums ON albums.artist_key = artists.artist_key
+LEFT JOIN tracks ON tracks.album_key = albums.album_key
+WHERE artist_name IS NOT NULL
+GROUP BY artist_name
+HAVING  COUNT(DISTINCT(track_name)) > 4
+ORDER BY avg_pop DESC
+LIMIT 5;
+
+--Q9  
+SELECT albums.album_key, album_name, AVG(track_popularity) AS avg_album_track_pop
+FROM albums
+LEFT JOIN tracks ON tracks.album_key = albums.album_key
+WHERE album_name IS NOT NULL
+GROUP BY albums.album_key
+HAVING AVG(track_popularity) > (SELECT avg(track_popularity)
+                                FROM tracks)
+
+--Q10
+-- count number of explicit and non explicit per artist and only return those who are >0 for both
+--first, count explict tracks per artsit
+WITH count_of_explicit_type AS(
+SELECT artist_name, 
+SUM(CASE WHEN tracks.is_explicit = TRUE THEN 1 ELSE 0 END) AS explicit_count,
+SUM(CASE WHEN tracks.is_explicit = FALSE THEN 1 ELSE 0 END) AS not_explicit_count
+FROM artists
+LEFT JOIN albums ON albums.artist_key = artists.artist_key
+LEFT JOIN tracks ON tracks.album_key = albums.album_key
+GROUP BY artist_name
+ORDER BY artist_name
+)
+
+SELECT artist_name
+FROM count_of_explicit_type
+WHERE explicit_count > 0 AND not_explicit_count > 0
+ORDER BY artist_name
+LIMIT 100
